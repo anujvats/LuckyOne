88a6f246d52efeba7e0b399ac13b7fdbabd66405
anuj.vats <anuj.vats@accenture.com>
2020-10-06 14:05:52 +0530

    Anuj: Project Setup

---
+++ LuckyOne/LuckyOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LuckyOne
+//
+//  Created by Anuj Vats on 06/10/20.
+//  Copyright © 2020 Anuj Vats. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
